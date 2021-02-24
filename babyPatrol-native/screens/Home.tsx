5cd1bc8b0fb84a905ac407962d341baaa03f87d8
Alexis Emperador <aemperador@techstyle.com>
2021-02-24 16:50:45 -0600

    adding initial react-native code

---
+++ babyPatrol-native/screens/Home.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+
+import HomeScreenInfo from '../components/HomeScreenInfo';
+import { Text, View } from '../components/Themed';
+
+export default function HomeScreen({ username }: { username:string }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, ${username}!</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <HomeScreenInfo path="/screens/TabOneScreen.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
---
+++ babyPatrol-native/types.tsx
@@ -0,0 +1,22 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  TabOne: undefined;
+  TabTwo: undefined;
+};
+
+export type HomeParamList = {
+  HomeScreen: undefined;
+};
+
+export type TabOneParamList = {
+  TabOneScreen: undefined;
+};
+
+export type TabTwoParamList = {
+  TabTwoScreen: undefined;
+};
