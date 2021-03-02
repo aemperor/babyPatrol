01addd267248f114111902009a944cbdfc6c518c
Alexis Emperador <aemperador@techstyle.com>
2021-03-01 21:23:55 -0600

    adding basic drawer navigation and hamburger icon

--- babyPatrol-native/navigation/DrawerNavigator.tsx
+++ babyPatrol-native/navigation/DrawerNavigator.tsx
@@ -1,46 +1,28 @@
-import { Ionicons } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
 import HomeScreen from '../screens/HomeScreen';
 import TabOneScreen from '../screens/TabOneScreen';
 import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList, TabOneParamList, TabTwoParamList, HomeParamList } from '../types';
+import { TabOneParamList, TabTwoParamList, HomeParamList } from '../types';
 
-const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+const Drawer = createDrawerNavigator();
 
-export default function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
-
+export default function DrawerNavigator() { 
   return (
-    <BottomTab.Navigator
-      initialRouteName="Home"
-      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint }}>
-      <BottomTab.Screen
+    <Drawer.Navigator
+      initialRouteName="Home">
+      <Drawer.Screen
         name="Home"
         component={HomeNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-        }}
       />
-      <BottomTab.Screen
+      <Drawer.Screen
         name="TabTwo"
         component={TabTwoNavigator}
-        options={{
-          tabBarIcon: ({ color }) => <TabBarIcon name="ios-code" color={color} />,
-        }}
       />
-    </BottomTab.Navigator>
+    </Drawer.Navigator>
   );
-}
-
-// You can explore the built-in icon families and icons on the web at:
-// https://icons.expo.fyi/
-function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string }) {
-  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
 }
 
 // Each tab has its own navigation stack, you can read more about this pattern here:
--- babyPatrol-native/screens/HomeScreen.tsx
+++ babyPatrol-native/screens/HomeScreen.tsx
@@ -1,24 +1,30 @@
 import * as React from 'react';
+import Constants from 'expo-constants';
 import { StyleSheet } from 'react-native';
 
+import BabyPatrolHeader from '../components/Header';
 import HomeScreenInfo from '../components/HomeScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function HomeScreen({ username }: { username:string }) {
+console.log(Constants.statusBarHeight);
+ 
+export default function HomeScreen({ username, navigation }: { username:string, navigation:any }) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome, ${username}!</Text>
+    <View style={styles.container}> 
+      <BabyPatrolHeader navigation={navigation} />
+      <Text style={styles.title}>Welcome, {username}!</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <HomeScreenInfo path="/screens/TabOneScreen.tsx" />
+      <HomeScreenInfo path="/screens/TabOneScreen.tsx" /> 
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.6,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: Constants.statusBarHeight, 
   },
   title: {
     fontSize: 20,
