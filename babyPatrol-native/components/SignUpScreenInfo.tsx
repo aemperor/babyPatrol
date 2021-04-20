1cbbaca133d85b765d16983290162916877bad82
Alexis Emperador <aemperador@techstyle.com>
2021-04-19 19:43:34 -0500

    re-styling the sign up screen with material ui

--- babyPatrol-native/components/SignUpScreenInfo.tsx
+++ babyPatrol-native/components/SignUpScreenInfo.tsx
@@ -1,36 +1,57 @@
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Headline, Subheading, TextInput, Button, Avatar } from 'react-native-paper';
 
-import { Text, View } from './Themed';
+import { View } from './Themed';
 
 import { styles } from '../styles/BaseStyle';
-import { TextInput } from 'react-native-gesture-handler';
 
 
 export default function SignUpScreenInfo() {
+  let username, email, password;
+  const signUpInfo : any = {
+    username: '',
+    email: '',
+    password: ''
+  };
 
   return (
     <View style={styles.container}>
-        <TextInput style={ styles.input }
-          placeholder="Create username"
-          returnKeyType="next"
-          blurOnSubmit={false}
-        />
-        <TextInput style={ styles.input }
-          placeholder="E-mail"
-          returnKeyType="next"
-          blurOnSubmit={false}
-        />
-        <TextInput style={ styles.input }
-          placeholder="Create password"
-          secureTextEntry
-          returnKeyType="next"
-          blurOnSubmit={false}
-        />
-        <TouchableOpacity style={ styles.button }>
-          <Text>Sign Up</Text>
-        </TouchableOpacity>
+      <Avatar.Image style={styles.signUpImage} size={150} source={require('../assets/images/babyPatrolLogo/babyPatrolLogo.002.png')} />
+      <Headline style={styles.headlineStyle}>Welcome to Baby Patrol!</Headline>
+      <Subheading style={styles.subHeadingStyle}>Let's get started...</Subheading>
+      <TextInput style={styles.textInput}
+        label='Username'
+        mode='outlined'
+        value={username}
+        onChangeText={username => signUpInfo.username = username}
+      />
+      <TextInput style={styles.textInput}
+        label='E-mail'
+        mode='outlined'
+        value={email}
+        onChangeText={email => signUpInfo.email = email}
+      />
+      <TextInput style={styles.textInput}
+        label='Password'
+        mode='outlined'
+        secureTextEntry={true}
+        value={password}
+        onChangeText={password => signUpInfo.password = password}
+      />
+      <Button
+        icon='account'
+        mode='contained' 
+        accessibilityLabel='sign up'
+        onPress={() => signUp(signUpInfo)}
+        compact={true}
+        style={styles.button}
+      >
+        Sign Up
+      </Button>
     </View>
   );
 }
 
+const signUp = (signUpInfo : object) => {
+  console.log(signUpInfo);
+}
