50eb130bfb30ad2b0aee18ccdeea3d1cdac25cc1
MikiEEE <mikemp1997@gmail.com>
2021-04-23 18:03:24 -0500

    Added cute little icons instead of the undefined music one to the bottom tab navigator

--- babyPatrol-native/navigation/BottomTabNavigation.tsx
+++ babyPatrol-native/navigation/BottomTabNavigation.tsx
@@ -5,6 +5,8 @@ import HomeScreen from '../screens/HomeScreen';
 
 const SignUpRoute = () => <SignUpScreen />;
 
+const LoginRoute = () => <SignUpRoute />;
+
 const HomeRoute = () => <HomeScreen username='person' />;
 
 const RecentsRoute = () => <Text>Recents</Text>;
@@ -12,15 +14,17 @@ const RecentsRoute = () => <Text>Recents</Text>;
 const BottomTabNavigation = () => {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
-    { key: 'signup', title: 'SignUp', icon: 'queue-music' },
+    { key: 'signup', title: 'SignUp', icon: 'pencil' },
     { key: 'home', title: 'Home', icon: 'home' },
     { key: 'recents', title: 'Recents', icon: 'history' },
+    { key: 'login', title: 'Login', icon: 'lock'},
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     signup: SignUpRoute,
     home: HomeRoute,
     recents: RecentsRoute,
+    login: LoginRoute,
   });
 
   return (
