d182c6503b25d3c261fd98e629cac453b0362f4f
MikiEEE <mikemp1997@gmail.com>
2021-04-26 16:06:51 -0500

    Moved login page style to /styles/LoginStyle

--- babyPatrol-native/components/LoginScreenInfo.tsx
+++ babyPatrol-native/components/LoginScreenInfo.tsx
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import {Text,StyleSheet,Dimensions} from "react-native"
-// import CheckBox from '@react-native-community/checkbox';
-import { Headline, Subheading, TextInput, Button, Avatar } from 'react-native-paper';
+import {Text} from "react-native"
+import { Headline, TextInput, Button} from 'react-native-paper';
 import { Checkbox } from 'react-native-paper';
 
 import { View } from './Themed';
 
 import { styles } from '../styles/BaseStyle';
-
+import {loginStyle} from '../styles/LoginStyle'
 
 
 
@@ -25,44 +24,45 @@ export default function LoginScreenInfo() {
     <View style={[styles.container,{alignItems:'center'}]}>
       <Headline style={styles.headlineStyle}>Login</Headline>
       
-      <TextInput style={LoginStyle.textInput}
+      <TextInput style={loginStyle.textInput}
         label='Username'
         mode='outlined'
         value={username}
         onChangeText={username => loginInfo.username = username}
       />
-      <TextInput style={LoginStyle.textInput}
+      <TextInput style={loginStyle.textInput}
         label='Password'
         mode='outlined'
         secureTextEntry={true}
         value={password}
         onChangeText={password => loginInfo.password = password}
         />
-        <View style={[LoginStyle.sideBySide,LoginStyle.color, {marginTop:20}]}>
-            <View style={[LoginStyle.color,LoginStyle.sideBySide,LoginStyle.checkboxView]}>
+        <View style={[loginStyle.sideBySide,loginStyle.color, {marginTop:20}]}>
+            <View style={[loginStyle.color,loginStyle.sideBySide,loginStyle.checkboxView]}>
                 <Checkbox
                     disabled={false}
                     status={willSaveUser ? "checked" : "unchecked"}
                     onPress={() => toggleWillSaveUser(!willSaveUser)}
                 />
-                <Text style={[LoginStyle.checkboxText,{alignContent:"flex-end"}]}>Remember Me</Text>
+                <Text style={[loginStyle.checkboxText,{alignContent:"flex-end"}]}>Remember Me</Text>
             </View>
 
-            <View style={[LoginStyle.color,LoginStyle.buttonView ]}>
+            <View style={[loginStyle.color,loginStyle.buttonView ]}>
                 <Button
                     icon='account'
                     mode='contained' 
                     accessibilityLabel='Login'
                     onPress={() => login(loginInfo,willSaveUser)}
                     compact={true}
-                    style={[LoginStyle.button]}
+                    style={[loginStyle.button]}
                 >
                     Login
                 </Button>
             </View>
         </View>
-            <Button style={[LoginStyle.googleButton]}
+            <Button style={[loginStyle.googleButton]}
             color="#FFFFFF"
+            onPress={() => googleLogin()}
             >
                 Login with Google
             </Button>
@@ -83,42 +83,3 @@ const googleLogin = () => {
     console.log('Google button has been pushed.');
 }
 
-
-
-const LoginStyle = StyleSheet.create({  
-    color: {
-        backgroundColor: '#FFFFFF',
-    },
-    button: {
-        width: '90%',
-        marginBottom: '10%',
-    },
-    sideBySide: {
-        flexDirection:'row',
-    },
-    checkboxView: {
-        alignItems:'baseline',
-        justifyContent:"center",
-        flex:1,    
-    },
-    checkboxText: {
-        textAlign: "center",
-        alignSelf:'center'
-    },
-    buttonView: {
-        flex:1,
-        justifyContent:"center",
-    },
-    googleButton: {
-        backgroundColor: '#4285F4',
-        width: '60%',
-        color: '#FFFFFF',
-    },
-    textInput: {
-        fontSize: 14,
-        backgroundColor: '#FFFFFF',
-        width: '90%',
-        justifyContent: 'flex-start',
-        marginTop: 20,
-    }
-});
---
+++ babyPatrol-native/styles/LoginStyle.tsx
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+export const loginStyle = StyleSheet.create({  
+    color: {
+        backgroundColor: '#FFFFFF',
+    },
+    button: {
+        width: '90%',
+        marginBottom: '10%',
+    },
+    sideBySide: {
+        flexDirection:'row',
+    },
+    checkboxView: {
+        alignItems:'baseline',
+        justifyContent:"center",
+        flex:1,    
+    },
+    checkboxText: {
+        textAlign: "center",
+        alignSelf:'center'
+    },
+    buttonView: {
+        flex:1,
+        justifyContent:"center",
+    },
+    googleButton: {
+        backgroundColor: '#4285F4',
+        width: '60%',
+        color: '#FFFFFF',
+    },
+    textInput: {
+        fontSize: 14,
+        backgroundColor: '#FFFFFF',
+        width: '90%',
+        justifyContent: 'flex-start',
+        marginTop: 20,
+    }
+});
+
